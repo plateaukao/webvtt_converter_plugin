af62958d0f1d1c6fbf6a46a53903b04d1e690351
Daniel Kao <daniel.kao@gmail.com>
2020-12-19 13:55:51 +0800

    1. make sentence pairing in two languages more accurate;2. remove h3 and use div instead for font size manipulation; 3. add break between silence to better know the boundary of a conversation.

--- convert.py
+++ convert.py
@@ -27,17 +27,24 @@ def convert_file(file_name, main_lang, sub_lang, output_file):
   # while loop all korean time captions
   index_main = 0
   index_sub = 0
+  # within how much duration, the captions are considered the same
+  threshold = 400
+  last_main_start = 0
   while index_main < len(vtt_main):
     while index_sub < len(vtt_sub):
       caption_main = vtt_main[index_main]
       caption_sub = vtt_sub[index_sub]
+      main_start = get_time(caption_main.start)
+      sub_start = get_time(caption_sub.start)
 
-      if (caption_main.start <= caption_sub.start):
-        write(output_file, "<h3>" + caption_main.text.replace("&lrm;","") + "</h3>")
+      if (main_start - threshold <= sub_start):
+        if main_start > last_main_start + 5000:
+          write(output_file, '<br/><br/>')
+        write(output_file, caption_main.text.replace("&lrm;",""))
+        last_main_start = main_start
         break
       else:
-        write(output_file, "<p>" + caption_sub.text.replace("&lrm;", ""))
-        write(output_file, "<p>")
+        write(output_file, '<div class="sub">' + caption_sub.text.replace("&lrm;", "") + "</div>")
         index_sub += 1
     index_main += 1
 
@@ -45,6 +52,13 @@ def convert_file(file_name, main_lang, sub_lang, output_file):
   while index_sub < len(vtt_sub):
     write(output_file, caption_sub.text)
     index_sub += 1
+
+# 00:00:00.000
+def get_time(time_str):
+  segments = time_str.split(':')
+  second = segments[2].split('.')[0]
+  millie_second = segments[2].split('.')[1]
+  return (int(segments[0]) * 3600 + int(segments[1]) * 60 + int(second)) * 1000 + int(millie_second)
 
 # main
 def get_film_name(vtt_dir):
@@ -72,6 +86,7 @@ def convert_webvtt_to_html(vtt_dir, main_lang, sub_lang, output_file):
   write(file, "<html>")
   title = get_film_name(vtt_dir)
   write(file, "<title>" + title + "</title>")
+  write(file, '<style> .sub { font-size: 70%;} </style>')
 
   for idx, vtt_file_name in enumerate(vtt_files_name):
     vtt_file = join(vtt_dir, vtt_file_name)
